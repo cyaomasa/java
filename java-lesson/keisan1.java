6d134a7178f25e0440e1e1a1bf43deb96377161a
cyaoma <cyao.masa.0924@gmail.com>
2023-09-21 08:15:39 +0000

    keisan1

---
+++ java-lesson/keisan1.java
@@ -0,0 +1,13 @@
+/** Keisan1.java */
+public class keisan1 {
+	public static void main ( String [] args ) {
+		int x = 50;
+		int y = 7;
+		int z = x * y;
+		System.out.println ( "x*y=" + z );
+		z= x / y;
+		System.out.println ( "x/y=" +z );
+		z = x % y;
+		System.out.println ( "x%y=" +z );
+	}
+}
