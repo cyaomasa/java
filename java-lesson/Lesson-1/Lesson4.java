e5c1edabd8cc81226b21a9ddb1faff9a5b421c19
cyaoma <cyao.masa.0924@gmail.com>
2023-09-22 05:44:18 +0000

    lesson1-4

---
+++ java-lesson/Lesson-1/Lesson4.java
@@ -0,0 +1,8 @@
+public class Lesson4{
+  public static void main(String[] args){
+    final double pi = 3.14;
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * pi);
+  }
+}
