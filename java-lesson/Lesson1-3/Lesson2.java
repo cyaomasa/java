6400fdcab2c90d2bca00b87384ab21064d51be30
cyaoma <cyao.masa.0924@gmail.com>
2023-09-27 08:51:09 +0000

    Lesson1-3

---
+++ java-lesson/Lesson1-3/Lesson2.java
@@ -0,0 +1,9 @@
+public class Lesson2 {
+  public static void main(String[] args) {
+    boolean doorClose = true;    // ここでtrueかfalseを代入
+    while (doorClose == true) {
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
+    }            /* 3行目がtrueの場合、このプログラムを実行すると */
+  }              /* 無限ループになるので、本文を参考に対応ください */
+}
