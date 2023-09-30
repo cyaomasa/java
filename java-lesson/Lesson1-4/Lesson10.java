80cb46c16edbd002234f024035705c262407d27d
cyaoma <cyao.masa.0924@gmail.com>
2023-09-30 00:58:18 +0000

    Lesson1-4

---
+++ java-lesson/Lesson1-4/Lesson10.java
@@ -0,0 +1,8 @@
+public class Lesson10 {
+  public static void main(String[] args) {
+    int[] array = {1, 2, 3};
+    array = null;
+    array[0] = 10;
+  }
+}
+　
---
+++ java-lesson/Lesson1-4/Lesson13.java
@@ -0,0 +1,4 @@
+int[] point = new int[4];
+double[] weights = new double[5];
+boolean[] answers = new boolean[3];
+String[] names = new String[3];
