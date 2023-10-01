d96d797ae020127aa1c24e34bd4cd7d866ac31a5
cyaoma <cyao.masa.0924@gmail.com>
2023-10-01 12:19:47 +0000

    Practice

---
+++ java-lesson/practice/Practice10.java
@@ -0,0 +1,8 @@
+public class Practice10 {
+  static public void main(String args[]){
+    String str = "English日本語aBc";
+    System.out.println(String.format("切り出し前の文字列 ： %s", str));
+    System.out.println("英小文字を大文字に変換 -> " + str.toUpperCase());
+    System.out.println("英大文字を小文字に変換 -> " + str.toLowerCase());
+  }
+}
