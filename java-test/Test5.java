d08c1f771aa9c731a629340294c2e96f8b06940a
cyaoma <cyao.masa.0924@gmail.com>
2023-10-14 12:23:29 +0000

    Test

---
+++ java-test/Test5.java
@@ -0,0 +1,10 @@
+// 1〜10の数字の偶数のみを大きい順に上から縦に並ぶように表示させる
+public class Test5 {
+  public static void main(String[] args) {
+    for (int i = 10; i >= 1; i--) {
+      if (i % 2 == 0) {
+        System.out.println(i);
+      }
+    }
+  }
+}
---
+++ java-test/Test7.java
@@ -0,0 +1,11 @@
+public class Test7 {
+  public static void main(String[] args) {
+    int sum = 0;
+    for (int i = 1; i <= 10; i++) {
+      if (i % 2 == 0) {
+        sum++;
+      }
+    }
+    System.out.println(sum);
+  }
+}
